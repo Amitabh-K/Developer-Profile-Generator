69a6ec7743c20cf301cd0dfcc271cd0ee835ba9d
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-08 15:49:09 -0500

    storing questions and resp

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const open = require("open")
 const generateHTML = require("./generateHTML")
 
 
+const data = {}
+const questions = [{
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username: ",
+},
+{
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["green", "blue", "pink", "red"],
+}];
 

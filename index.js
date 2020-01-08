ef75ccc27b4b5629655d5c2f0e030cb3c01d3fd4
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-08 13:56:27 -0500

    npm pakages

--- index.js
+++ index.js
@@ -1,67 +1,32 @@
-// NPM Packages
-const fs = require('fs');
-const axios = require('axios');
-const inquirer = require('inquirer');
-const open = require('open');
-
-const convertFactory = require('electron-html-to');
-const conversion = convertFactory({
-  converterPath: convertFactory.converters.PDF
-});
-
-const generateHTML = require('./generateHTML');
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const puppeteer = require("puppeteer");
+const genHTML = require("./generateHTML");
 
 
-const questions = [
-    {
-      type: 'input',
-      name: 'username',
-      message: 'Enter a GitHub username:'
-    },
-    {
-      type: 'list',
-      name: 'color',
-      message: 'Which color do you prefer?',
-      choices: [
-        'green',
-        'blue',
-        'pink',
-        'red'
-      ]
-    }
-  ];
 
-function writeToFile(fileName, data) {
+// Take Username and Color as an input using inquirer
+
+
+// Store Github  json and user input
+
+
+
+//Match n map the github json with objects listed in generateHTML.js file
+
+ // call to get the stars 
+
  
-}
 
 
 
-inquirer
-  .prompt(questions)
-  .then(answers => {
-    // Destructure answers
-    const { username, color } = answers;
-    // Make two requests to GitHub based on username
-    Promise.all([
-      axios.get(`https://api.github.com/users/${username}`),
-      axios.get(`https://api.github.com/users/${username}/starred`)
-    ]).then(response => {
-      // Destructure response #1 which contains most of the data
-      const { data } = response[0];
-      // Response #2 is just to get the number of starred repos
-      data.stars = response[1].data.length;
-      // Convert to PDF passing in data and color args
-      conversion({
-        html: generateHTML(data, color)
-      }, (err, result) => {
-        if (err) throw err;
-        result.stream.pipe(fs.createWriteStream('output/profile.pdf'));
-        conversion.kill();
-        // Open resulting PDF
-        open('output/profile.pdf');
-      });
-    });
-  });
+// try and catch statement that knows how to handle the situation that the github user has no stars, and takes that situation to set the stars to 0
+    
+  
 
 
+// function ties in the color list to the color list in provided generateHTML.js file
+
+
+// generate the PDF and send it go the results folder in the same directory

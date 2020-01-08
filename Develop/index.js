68b737a847a56e7d2966636d3cb585ec6f5d3f67
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-08 13:08:22 -0500

    adding inquirer and axios

--- Develop/index.js
+++ Develop/index.js
@@ -30,4 +30,31 @@ function writeToFile(fileName, data) {
 function init() {
 }
 
+inquirer
+  .prompt(questions)
+  .then(answers => {
+    // Destructure answers
+    const { username, color } = answers;
+    // Make two requests to GitHub based on username
+    Promise.all([
+      axios.get(`https://api.github.com/users/${username}`),
+      axios.get(`https://api.github.com/users/${username}/starred`)
+    ]).then(response => {
+      // Destructure response #1 which contains most of the data
+      const { data } = response[0];
+      // Response #2 is just to get the number of starred repos
+      data.stars = response[1].data.length;
+      // Convert to PDF passing in data and color args
+      conversion({
+        html: generateHTML(data, color)
+      }, (err, result) => {
+        if (err) throw err;
+        result.stream.pipe(fs.createWriteStream('output/profile.pdf'));
+        conversion.kill();
+        // Open resulting PDF
+        open('output/profile.pdf');
+      });
+    });
+  });
+
 
